d6ea1017cb7814f27c849d56c6180f625fd85704
Antonín Vondrovic <antonin.vondrovic@educanet.cz>
2021-04-26 09:19:17 +0200

    takhle to nemá fungovat

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let cards = ["😂", "🥰", "🤔", "🙂", "😍", "😎", "🤩", "😃"]
-let cardsdoubled = cards.concat(cards)
+let cards = ['😂', '🥰', '🤔', '🙂', '😍', '😎', '🤩', '😃'] // emojis
+let cardsdoubled = cards.concat(cards) // doubles the cards
 let board;
 console.log(cardsdoubled)
 
-let init = function () {
+let init = function () { // init
     board = document.getElementById('board')
     shuffleFunc()
     htmlAdd()
 }
 
-let shuffleFunc = function () {
+let shuffleFunc = function () { // Shuffles the cards, duh
     for (let i = 0; i < cardsdoubled.length; i++) {
         let shuffle = Math.floor(Math.random() * i)
         let j = cardsdoubled[i]
@@ -22,9 +22,18 @@ let htmlAdd = function () {
     for (let i = 0; i < cardsdoubled.length; i++) {
         let div = document.createElement('div')
         div.innerText = cardsdoubled[i]
-        div.className = 'piece'
+        div.className = 'piece hidden'
+        div.id = `piece${i+1}`
+        div.onclick = revealCard
         board.appendChild(div)
     }
+}
+
+let revealCard = function () {
+    let piece = document.getElementById('piece1')
+
+    piece.className = 'piece revealed'
+    board.appendChild(piece)
 }
 
 
